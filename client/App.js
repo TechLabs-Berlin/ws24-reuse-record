39747b6a8685527b532dd0d3932743c76d308dcf
Pam <prati.ma@hotmail.com>
2024-03-16 12:28:08 +0100

    Fix error

--- client/App.js
+++ client/App.js
@@ -1,34 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, Text, TextInput, View } from "react-native";
-import {Camera} from 'expo-camera'
-const [startCamera, setStartCamera] = React.useState(false);
-
-
-const __startCamera = async () => {
-  const {status} = await Camera.requestPermissionsAsync()
-if(status === 'granted'){
- // do something
-  // start the camera
-  setStartCamera(true)
-
-}else{
- Alert.alert("Access denied")
-}
+import { Camera } from "expo-camera";
+import { useState } from "react";
 
 export default function App() {
+  const [cameraOpen, setCameraOpen] = useState(false);
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === "granted") {
+      // start the camera
+      setCameraOpen(true);
+    } else {
+      Alert.alert("Access denied");
+    }
+  };
+
   return (
     <View className="my-auto px-10">
-      <Text className="text-3xl bold mb-5">Login</Text>
-      <Text className="">Username:</Text>
-      <TextInput className=" border p-2" placeholder="Enter your username" />
-      <Text>Password:</Text>
-      <TextInput
-        className="mb-5 border p-2"
-        placeholder="Enter your password"
-        secureTextEntry
-      />
-      <Button title="Take a picture " color="#ac4" />
-      <Camera></Camera>
+      {cameraOpen ? (
+        <>
+          <Camera
+            className="block w-full h-[80%]"
+            ref={(r) => {
+              camera = r;
+            }}
+          ></Camera>
+          <Button title="Click!" color="#ac4" />
+        </>
+      ) : (
+        <>
+          <Button title="Open Camera" color="#ac4" onPress={__startCamera} />
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
