7b420b64b742347cbe9c2fc2da271413658a4f14
„Jannik0s“ <jannik@oslender.de>
2024-03-28 00:16:10 +0100

    mvp working

--- server/calc.js
+++ server/calc.js
@@ -1,70 +1,120 @@
-// window?.grid?.frame?.width
-// window?.grid?.frame?.height
-// window?.grid?.count?.x
-// window?.grid?.count?.y
-// window?.grid?.factor?.x
-// window?.grid?.factor?.y
-// window?.grid?.cells[i]?.frame?.profile?.depth
-
-
-
-// // Grid
-
-// seedWindow.grid.count.x = 3;
-// seedWindow.grid.count.y = 3;
-
-
-// thisStep = lastStep;
-
-// lastStep.grid.frame.width = 120;
-// lastStep.grid.frame.height = 240;
-
-// lastStep.grid.cells[0].type = "fixed";
-
-// const thisStep = windowCalc(lastStep)
-
 const _ = require('lodash');
 
-// export 
-function windowCalc(input) {
+export function windowCalc(input) {
 
-    // Default without Cells
+    // Functions
+    function adjustArray(count, factor, add) {
+        if (count > factor.length) {
+            const missing = count - factor.length;
+            for (let i = 0; i < missing; i++) {
+                factor.push(add);
+            }
+        } else if (count < factor.length) {
+            const excess = factor.length - count;
+            for (let i = 0; i < excess; i++) {
+                factor.pop();
+            }
+        }
+    }
 
+
+    // GRID LEVEL
+
+    // Default
     const defaultGrid = {
         grid: {
-            "width": 100,
-            "height": 100,
+            "width": null,
+            "height": null,
             "count": {
                 "x": 1,
-                "y": 1
+                "y": 1,
+                "total": null
+            },
+            "factor": {
+                "x": [1],
+                "y": [1]
             },
             "frame": {
+                "width": 104,
+                "height": 104,
                 "material": "pvc",
                 "surface": "natural",
                 "colour": "white",
                 "profile": {
                     "width": 8,
                     "offsetOutXY": 2,
+                    "offsetInXY": null,
                     "depth": 6
                 }
             },
+            "cells": []
         }
     }
 
     // Merges the input object with the default object to set default values for missing properties
     const result = _.merge({}, defaultGrid, input);
 
-    if (input?.grid?.count && input?.grid?.cells?.length > 0) {
-        input.gird.count.x * input.grid.count.y
-    } else {
+    // Calculations
+    result.grid.width = result.grid.frame.width - result.grid.frame.profile.offsetOutXY * 2;
+    result.grid.height = result.grid.frame.height - result.grid.frame.profile.offsetOutXY * 2;
 
+    result.grid.count.total = result.grid.count.x * result.grid.count.y;
+
+    result.grid.frame.profile.offsetInXY = result.grid.frame.profile.width - result.grid.frame.profile.offsetOutXY;
+
+    // Factor x & y
+    adjustArray(result.grid.count.x, result.grid.factor.x, 1);
+    adjustArray(result.grid.count.y, result.grid.factor.y, 1);
+
+
+    // CELL LEVEL
+
+    // Default
+    const defaultCell = {
+        "width": null,
+        "height": null,
+        "type": "openable",
+        "frame": {
+            "width": null,
+            "height": null,
+            "profile": {
+                "width": 8,
+                "offsetOutXY": 2,
+                "offsetInXY": null,
+                "depth": 6
+            }
+        },
+        "glass": {
+            "width": null,
+            "height": null,
+            "type": "double"
+        }
     }
 
 
+    adjustArray(result.grid.count.total, result.grid.cells, defaultCell);
 
-    // // !!!CHANGE: dependent on count
-    // window?.grid?.factor?.x ?? [1];
-    // window?.grid?.factor?.y ?? [1];
+    // Calculations
+    for (let cell of result.grid.cells) {
+        // for now without factor
+        cell.width = result.grid.width / result.grid.count.x;
+        cell.height = result.grid.height / result.grid.count.y;
+    }
+
+
+    // if (result.grid.count.total > result.grid.cells.length) {
+    //     const missing = result.grid.count.total - result.grid.cells.length;
+    //     for (let i = 0; i < missing; i++) {
+    //         result.grid.cells.push(defaultCell);
+    //     }
+    // } else if (result.grid.count.total < result.grid.cells.length) {
+    //     const excess = result.grid.cells.length - result.grid.count.total;
+    //     for (let i = 0; i < missing; i++) {
+    //         result.grid.cells.pop();
+    //     }
+    // }
+
+
 
     // for (let i = 0; i < window.gird.count.x * window.grid.count.y; i++) {
     //     window?.grid?.cells[i]?.frame?.profile?.depth ?? 8;
@@ -72,10 +122,6 @@ function windowCalc(input) {
 
 
     // // Defaults
-
-    // window.grid.frame.profile.width = 8;
-    // window.grid.frame.profile.offsetOutXY = 2;
-    // window.grid.frame.profile.depth = 8;
 
     // for (let i = 0; i < window.gird.count.x * window.grid.count.y; i++) {
     //     window.grid.cells[i].frame.profile.width = 8;
@@ -115,18 +161,25 @@ function windowCalc(input) {
 }
 
 
-let inputObject = {
-    grid: {
-        width: 50,
-        height: 50
-    }
-};
+// let inputObject = {
+//     grid: {
+//         count: {
+//             x: 2,
+//             y: 2
+//         },
+//         frame: {
+//             width: 50,
+//             height: 50
+//         }
+//     }
+
+// };
 
 
 
-const resultObject = windowCalc(inputObject);
-console.log(inputObject);
-console.log(resultObject);
+// const resultObject = windowCalc(inputObject);
+// console.log(inputObject);
+// console.log(JSON.stringify(resultObject, null, 2));
 
 
 
