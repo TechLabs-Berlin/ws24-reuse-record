d18374e2fc327429d56f4f99e5cbff69a11b7083
pynat <natlu@gmx.de>
2024-03-29 12:55:38 +0100

    remove data

--- NOmongo_data.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-def get_window_data():
-    client = pymongo.MongoClient("mongodb://username:password@hostname:port/database")
-    db = client['your_database_name']
-    collection = db['your_collection_name']
-    window_configs = collection.find({})
-    return window_configs
